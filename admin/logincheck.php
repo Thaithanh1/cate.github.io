a11eacc62fb4dea129241df2e0590cbada0267ae
Thaithanh1 <77682579+Thaithanh1@users.noreply.github.com>
2021-04-14 15:46:08 +0700

    Update logincheck.php

--- admin/logincheck.php
+++ admin/logincheck.php
@@ -1,24 +1,24 @@
 	<?php
 session_start();
 
-$con = mysqli_connect('localhost', 'root' );
+$con = pg_connect('ec2-34-225-103-117.compute-1.amazonaws.com', 'eophfavabkrorx' );
 if($con){
 	echo"connection successful";
 }else{
 	echo "no connection";
 }
 
-$db = mysqli_select_db($con, 'db_tune');
+$db = mysqli_select_db($con, 'dcg72tr1l83dd3');
 
 if(isset($_POST['loginSubmit'])){
 	$username=$_POST['name'];
 	$password=$_POST['pass'];
 
-	$sql="select * from tb_admin where a_name='$username'
-	 and a_password='$password'";
-	 $query= mysqli_query($con, $sql);
+	$sql="select * from account where username='$username'
+	 and password='$password'";
+	 $query= pg_query($con, $sql);
 
-	 $row= mysqli_num_rows($query);
+	 $row= pg_num_rows($query);
 	 	if($row == 1){
 	 		echo "login successful";
 	 		$_SESSION['name'] =$username;
